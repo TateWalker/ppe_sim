ec5bf529fb1097f51f151e1c3884c26e8c9cdf8d
Tate Walker <tatewalker@comcast.net>
2020-02-11 13:31:24 -0600

    added classes + functions

---
+++ mainSys.py
@@ -0,0 +1,158 @@
+import itertools
+import random
+import simpy
+
+
+class PPE:
+
+	def __init__(self, env):
+		self.env = env
+
+	def alert():
+		print('Alert alert')
+
+	def reboot():
+		print('Set everything back to defaults')
+
+	def systemsOn():
+		print('Sent \'On\' signal to each subsystem. ')
+
+class Subsystem:
+	'''
+	Parent class for all subsystems. Defines default values for each subsystem.
+	'''
+
+	powered_on = False
+	power_usage = 0.0
+	is_stable = False
+
+
+	def reboot():
+		print('Received reboot signal')
+
+	def powerOn():
+		print('Make it turn on')
+
+	def getReport(subsystem):
+		print('Get report from subsystem')
+
+	def sendReport(subsystem):
+		print('Send report to subsystem')
+
+
+class Communications(Subsystem):
+
+	def __init__(self):
+		self.signal_strength = 0.0 #Mbps
+		self.connected = False
+		self.encrypted = False
+
+
+	#getters
+
+	def getMessage(): #read from queue
+
+	def getSignalStrength():
+
+	def getReport(subsystem):
+
+
+	#setters
+
+	def setPowerDraw(power_level):
+
+	#utils
+
+	def sendMessage(): #send to nowhere (NASA)
+
+	def encryptData():
+
+		encrypted = True
+		print('Encrypted shit')
+
+		return encrypted
+
+
+class Power(Subsystem):
+
+	def __init__(self):
+		self.total_power = 100 # kW ??
+		self.available_power = self.total_power
+		self.generating_power = False
+		self.generation_rate = 0.0
+		self.eco_mode = False
+		self.battery_level = 100 # percent
+
+
+	#getters
+
+	def getBattery():
+
+	
+
+
+	#setters
+
+	def togglePowerSaving():
+		print("Changing power saving from {} to {}.".format(eco_mode,not(eco_mode)))
+		return not(eco_mode)
+
+	#utils
+
+	def reroutePower():
+
+
+
+class GNC(Subsystem):
+
+	def __init__(self):
+		self.pos_target = np.empty([1 3])
+		self.pos_error = np.empty([1 3])
+
+	def verifyManeuver():
+		print('wat')
+
+	def impulseApplied():
+		print('wat')
+
+
+class Propulsion(Subsystem):
+
+	def __init__(self):
+		self.prop_mode = "FULL SPEED AHEAD"
+		self.main_prop = True
+		self.main_prop_burn_time = 0.0
+		self.cathode_power = False
+
+	#getters
+
+	def verifyThrust():
+		if thrust:
+			print('Thrusting')
+		else:
+			print('Not thrusting')
+	
+	#setters
+
+	def toggleCathode():
+		print("Changing cathode power from {} to {}.".format(cathode_power,not(cathode_power)))
+		return not(eco_mode)
+
+	def fireMain():
+		print('Firing main')
+		self.main_prop = True
+		#increase burn time
+
+
+class Thruster:
+
+	activated = False
+	burnTime = 0.0
+
+	#getters
+
+	def isActive():
+		return activated
+
+	def getBurnTime():
+		return burnTime
