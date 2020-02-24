7b65632d85334fabb805e668d00fc91f352f52e7
Kendall <kendallpmares@gmail.com>
2020-02-23 22:54:44 -0600

    hello

---
+++ gnc/gncSim.py
@@ -0,0 +1,62 @@
+class GNC:
+
+    def __init__(self):
+        self.powerOn = False
+        self.pos_target = np.empty([3])
+        self.pos_error = np.empty([3])
+        self.current_attitude = np.empty([3])
+        self.burn_time = 0.0
+        self.thrustersOn = np.empy([16]) # 16 thrusters needed   placeholder until we figure out exactly how many thrusters
+        self.thrustMag = 5 # N   placeholder
+        self.angDisp = np.empty([3])
+
+
+
+
+# setters
+    def watchAttitude(self):
+        for i in range(len(pos_error)):
+            if self.pos_error[i] > 0.087266: #rad (5 deg)
+                for i in range(len(thrustersOn[1])):
+                  # set roll thrusters equal to 1
+                  self.thrustersOn[2] = 1
+                  self.thrustersOn[4] = 1 # etc.... to be decided later
+
+
+    def FireThruster(self):
+        for i in range(len(thrustersOn)):
+            if self.thrustersOn[i] == 1:
+                 self.burn()
+
+
+    def burn(self):
+        for i in range(len(pos_error)):
+            while self.pos_error[i] > 0.087266:
+                self.burn_time += 0.1
+              # these should all be 3dVectors
+              # torque[i] = r cross F    where r is 3dVector dist from thrust to origin of cube
+              # alpha[i] = inv(I_mat)*torque     must find matrix of inertia for this (but its a cube so its super easy)
+              # omega[i] = alpha*self.burn_time       get angular position by integrating
+                for i in range(len(angDisp)):
+                    print('Angular Displacement: {}'.format(self.angDisp[i]))
+                    self.pos_error[i] = self.pos_error[i] - angDisp[i]
+        for j in range(len(thrustersOn)):
+            self.thrustersOn[j] = 0 # turn thrusters off
+
+
+# getters
+
+    def checkAttitude(self):
+        print('Attitude Error: {}'.format(self.pos_error))
+
+    def gncReport(self):
+        print('\n------------GNC Report------------\n')
+        for i in range(len(thrustersOn)):
+            print('We firing boyz? {}'.format(self.thrustersOn[i]))
+        for i in range(len(angDisp)):
+            print('Angular Displacement: {}'.format(self.angDisp[i]))
+            print('Angular velocity: {}'.format(self.omega[i]))
+            print('Position Error: {}'.format(self.pos_error[i]))
+
+        print('\n---------------------------------------------\n')
+
--- propulsion/EngineSim.py
+++ propulsion/EngineSim.py
@@ -25,44 +25,73 @@ meltPt = df['MeltingPt'].values.tolist()  # celsius
 cHeat = df['cHeat'].values.tolist()  # J/(g*K)
 
 
-# placeholders
-powerOn = True
-power = 12.5 # kW   remember to divide by 4 (one for each thruster)
-volts = 300  # volts
 
 class IonProp:
 
-    def __init__(self, env):
-        self.env = env
-        #self.FireProcess = env.process(self.FireMainThrust(self, env, kg_ionizd, power))
-        #self.kg_ionizd = env.process(self.kg_ionizd(self, env, power))
-        #self.watchAttitude = env.process(self.watchAttitude(self, env, ** bool is_stable **))
+    def __init__(self):
+        self.powerOn = False
+        self.main_prop_burn_time = 0.0 # s
+        self.MassFlow = 0.0 # kg/s
+        self.massKg = 0.0 # kg
+        self.power = 0.0 # kW
+        self.volts = 0.0 # volts
+        self.DeltaVel = 0.0 # m/s
+        self.thrust = 0.0 # N
+        self.power_usage = 0.0
+        self.SetPowerDraw = 40 # change later
 
-    def FireMainThrust(self, kg_ionizd, powerOn, power, volts): # use massflow or kg_ionizd here?
-        while powerOn:
-            VelOut = pow((2 * volts * q) / kg_ionizd[1], 0.5) # m/s
-            massInit = 7500  # initial mass of PPE in kg
-            DeltaVel = VelOut*log(massInit/(massInit-kg_ionizd[0])) # is my notation correct here?    units: m/s
-            thrust = MolMass * massPro * VelOut * ((power * 1000) / volts) * cou  # Newtons  we may not need this value
-            if powerOn == False:
-                 break
-        return DeltaVel, thrust
 
-    def kg_ionizd(self, env, powerOn, power):
+
+# setters
+
+    def MainPropOn(self):
+        self.powerOn = True
+        print('\nIon propultion on')
+        self.power = 12.5
+        self.volts = 300
+        self.setPowerDraw()
+        self.kg_ionized(simpy.Environment)
+        print('\nPreparing to fire...')
+        self.FireMainProp()
+
+    def setPowerDraw(self):
+        self.power_usage = self.power
+
+
+    def kg_ionized(self, env):
         # intrinsic properties
-        massKg = (MolMass / n) * 0.001  # kg
-        molIonz = power / FIE  # mol/s
+        self.massKg = (MolMass / n) * 0.001  # kg
+        molIonz = self.power / FIE  # mol/s
         MassIonz = molIonz * MolMass * 0.001  # kg/s
         IonizProb = env.timeout(random.randint(0.40,0.60))
-        MassFlow = MassIonz*IonizProb  # kg/s
-
-        return MassFlow, massKg
-
-   # def watchAttitude(self, ** bool is_stable ** ):
-        # put in code to fire attitude thrusters
+        self.MassFlow = MassIonz*IonizProb  # kg/s
+        print('Kg Ionized: {}'.format(self.massKg))
+        print('Mass flow rate: {}'.format(self.MassFlow))
 
 
+    def FireMainProp(self): # use massflow or kg_ionizd here?
+        while self.powerOn:
+            VelOut = pow((2 * self.volts * q) / self.massKg, 0.5) # m/s
+            massInit = 7500  # initial mass of PPE in kg
+            self.DeltaVel = VelOut*log(massInit/(massInit - self.MassFlow)) # is my notation correct here?    units: m/s
+            self.thrust = MolMass * massPro * VelOut * ((self.power * 1000) / self.volts) * cou  # Newtons  we may not need this value
+            if not self.powerOn:
+                 break
+        print('Delta V: {}'.format(self.DeltaVel))
+        print('Thrust: {}'.format(self.thrust))
 
-env = simpy.Environment()
-go = IonProp(env)
-env.run(until=500)
+
+
+# getters
+    def getReport(self):
+        print('\n------------Ion Propulsion Report------------\n')
+        print('We firing boyz? {}'.format(self.powerOn))
+        print('Power draw: {}'.format(self.power))
+        self.kg_ionized(simpy.Environment)
+        self.FireMainProp()
+        print('\n---------------------------------------------\n')
+
+
+#env = simpy.Environment()
+#go = IonProp(env)
+#env.run(until=500)
