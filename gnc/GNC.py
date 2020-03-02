d93e832609a2590c7aba916c761b22f3d8c6f1fc
Kendall <kendallpmares@gmail.com>
2020-03-02 11:20:23 -0600

    update

--- gnc/GNC.py
+++ gnc/GNC.py
@@ -1,9 +1,12 @@
-import pandas
+import pandas as pd
 import numpy as np
+from scipy.spatial.transform import Rotation
 
-data = pandas.read_csv(r'/Users/kendallmares/ppe_sim/gnc/ppe_mass_prop.csv')
-df = pandas.DataFrame(data, columns=['Mass', 'CoMX', 'CoMY', 'CoMZ', 'Ixx', 'Iyy', 'Izz', 'Ixy', 'Ixz', 'Iyz'])
+data = pd.read_csv (r'/Users/kendallmares/ppe_sim/gnc/ppe_mass_prop.csv')
+df = pd.DataFrame(data, columns=['Mass', 'CoMX', 'CoMY', 'CoMZ', 'Ixx', 'Iyy', 'Izz', 'Ixy', 'Ixz', 'Iyz'])
 
+thrusters = pd.read_csv(r'/Users/kendallmares/ppe_sim/gnc/PPE_thruster_table_pythonformat.csv')
+th = pd.DataFrame(thrusters, columns=['Xpos', 'Ypos', 'Zpos', 'Xdir', 'Ydir', 'Zdir', 'ThrustMag'])
 
 ppe_mass = df['Mass'].values
 COMX = df['CoMX'].values
@@ -16,26 +19,46 @@ Ixy = df['Ixy'].values
 Ixz = df['Ixz'].values
 Iyz = df['Iyz'].values
 
-InertiaMat = numpy.array([[df['Ixx'].values, df['Ixy'].values, df['Ixz'].values], [df['Ixy'].values, df['Iyy'].values, df['Iyz'].values], [df['Ixz'].values, df['Iyz'].values, df['Izz'].values]])
+th_Xpos = th['Xpos'].values
+th_Ypos = th['Ypos'].values
+th_Zpos = th['Zpos'].values
+th_Xdir = th['Xdir'].values
+th_Ydir = th['Ydir'].values
+th_Zdir = th['Zdir'].values
+th_mag = th['ThrustMag'].values
 
-print(InertiaMat)
+# rot = Rotation.from_euler('z', 90, degrees=True)
+
+InertiaMat = np.array([[df['Ixx'].values, df['Ixy'].values, df['Ixz'].values], [df['Ixy'].values, df['Iyy'].values, df['Iyz'].values], [df['Ixz'].values, df['Iyz'].values, df['Izz'].values]])
+
+# initial rotation matrix, this is a placeholder
+Init_CMat = np.array([[-0.0000000, -0.7071068,  0.7071068],[0.7071068, -0.5000000, -0.5000000], [0.7071068,  0.5000000,  0.5000000]])
+
+# yet another placeholder
+Init_position = np.array([[1, 1, 1], [1,1,1], [1,1,1]])
 
 class GNC:
 
     def __init__(self):
         self.powerOn = False
-        self.pos_target = np.empty([1, 3])
-        self.pos_error = np.empty([1, 3])
-        self.current_attitude = np.empty([1, 3])
+        self.pos_target = np.empty([3])
+        self.pos_error = np.empty([3])
+        self.current_attitude = Init_CMat # to be updated
         self.burn_time = 0.0
-        self.thrustersOn = np.empy([1, 16]) # 16 thrusters needed   placeholder until we figure out exactly how many thrusters
-        self.thrustMag = 5 # N   placeholder
-        self.angDisp = np.empty([1, 3])
-        self.current_position([1, 3])
-        self.thrusterPosition([16, 3])
-        self.thrusterDirection([16, 3])
+        self.thrustersOn = np.empy([32])
+        self.thrustMag = th_mag # N
+        self.angDisp = np.empty([3])
+        self.current_position = Init_position # to be updated
+        self.thrusterPosition = np.array([th_Xpos, th_Ypos, th_Zpos])
+        self.thrusterDirection = np.array([th_Xdir, th_Ydir, th_Zdir])
         self.angDisp = np.empty([3])
         self.power_usage = 0
+        self.forceVecFill = np.empty([3])
+        self.forceVec = np.empty([3])
+        self.torqueVec = np.empthy([3])
+        self.alphaVec = np.empthy([3])
+        self.omegaVec = np.empthy([3])
+        self.angDisp = np.empthy([3])
 
 
 # setters
@@ -44,16 +67,16 @@ class GNC:
         self.powerOn = True
 
     def watchAttitude(self):
-        for i in range(len(pos_error)):
+        for i in range(len(self.pos_error)):
             if self.pos_error[i] > 0.087266: #rad (5 deg) this is a placeholder
-                for i in range(len(thrustersOn[1])):
+                for i in range(len(self.thrustersOn[1])):
                   # set roll thrusters equal to 1
                   self.thrustersOn[2] = 1
                   self.thrustersOn[4] = 1 # etc.... to be decided later
 
 
     def FireThruster(self):
-        for i in range(len(thrustersOn)):
+        for i in range(len(self.thrustersOn)):
             while self.pos_error[i] > 0.087266:
                 if self.thrustersOn[i] == 1:
                      self.burn()
@@ -61,24 +84,18 @@ class GNC:
 
     def burn(self):
         # these should all be 3dVectors
-        forceVecFill = np.empty([1, 3])
-        forceVec = np.empty([1, 3])
-        torqueVec = np.empthy([1, 3])
-        alphaVec = np.empthy([1, 3])
-        omegaVec = np.empthy([1, 3])
-        angDisp = np.empthy([1, 3])
-        for i in range(len(pos_error)):
+        for i in range(len(self.pos_error)):
             self.burn_time += 0.1
-            forceVecFill[i] = self.current_position[i] + self.thrusterPosition[i] # transformation matrix multiplication get Inertia mat of moon
-            forceNorm = numpy.linalg.norm(forceVecFill)
-            forceVec[i] = self.thrustMag * forceVecFill[i]/forceNorm
-            torqueVec[i] = numpy.cross(thrusterPosition[i, :], forceVec[i])        #   transpse this where r is 3dVector dist from thrust to origin of cube
-            alphaVec[:] = numpy.linalg.inv(InertiaMat) * torqueVec[:]    #must find matrix of inertia for this (but its a cube so its super easy)
-            omegaVec[i] = alphaVec[i] * self.burn_time
-            angDisp[i]= omegaVec[i] * self.burn_time # this is an average value
-        for i in range(len(angDisp)):
+            self.forceVecFill[i] = self.current_position[i] + self.thrusterPosition[i] # transformation matrix multiplication get Inertia mat of moon
+            forceNorm = np.linalg.norm(self.forceVecFill)
+            self.forceVec[i] = self.thrustMag * self.forceVecFill[i]/forceNorm
+            self.torqueVec[i] = np.cross(self.thrusterPosition[i, :], self.forceVec[i])        #   transpse this where r is 3dVector dist from thrust to origin of cube
+            self.alphaVec[:] = np.linalg.inv(InertiaMat) * self.torqueVec[:]    #must find matrix of inertia for this (but its a cube so its super easy)
+            self.omegaVec[i] = self.alphaVec[i] * self.burn_time
+            self.angDisp[i]= self.omegaVec[i] * self.burn_time # this is an average value
+        for i in range(len(self.angDisp)):
             print('Angular Displacement: {}'.format(self.angDisp[i]))
-            self.pos_error[i] = self.pos_error[i] - angDisp[i]
+            self.pos_error[i] = self.pos_error[i] - self.angDisp[i]
 
 
 
@@ -93,9 +110,9 @@ class GNC:
 
     def gncReport(self):
         print('\n------------GNC Report------------\n')
-        for i in range(len(thrustersOn)):
+        for i in range(len(self.thrustersOn)):
             print('We firing boyz? {}'.format(self.thrustersOn[i]))
-        for i in range(len(angDisp)):
+        for i in range(len(self.angDisp)):
             print('Angular Displacement: {}'.format(self.angDisp[i]))
             print('Angular velocity: {}'.format(self.omega[i]))
             print('Position Error: {}'.format(self.pos_error[i]))
