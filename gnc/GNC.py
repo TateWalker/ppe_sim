3713ca89489d2c3cd041e1f0bca4de980b25d00e
Kendall <kendallpmares@gmail.com>
2020-04-06 21:52:00 -0500

    clean house

--- gnc/GNC.py
+++ gnc/GNC.py
@@ -2,10 +2,25 @@ import pandas as pd
 import numpy as np
 import random
 
+
+
+data = pd.read_csv (r'/Users/kendallmares/ppe_sim/gnc/ppe_mass_prop.csv')
+df = pd.DataFrame(data, columns=['Mass', 'CoMX', 'CoMY', 'CoMZ', 'Ixx', 'Iyy', 'Izz', 'Ixy', 'Ixz', 'Iyz'])
+
 thrusters = pd.read_csv(r'/Users/kendallmares/ppe_sim/gnc/PPE_thruster_table_pythonformat.csv')
 th = pd.DataFrame(thrusters, columns=['Xpos', 'Ypos', 'Zpos', 'Xdir', 'Ydir', 'Zdir', 'ThrustMag'])
 
-# we could use these to calculate more exact torque values 
+ppe_mass = df['Mass']
+COMX = df['CoMX']
+CoMY = df['CoMY']
+CoMZ = df['CoMZ']
+Ixx = df['Ixx']
+Iyy = df['Iyy']
+Izz = df['Izz']
+Ixy = df['Ixy']
+Ixz = df['Ixz']
+Iyz = df['Iyz']
+
 th_Xpos = th['Xpos'].values
 th_Ypos = th['Ypos'].values
 th_Zpos = th['Zpos'].values
@@ -13,6 +28,8 @@ th_Xdir = th['Xdir'].values
 th_Ydir = th['Ydir'].values
 th_Zdir = th['Zdir'].values
 th_mag = th['ThrustMag'].values
+
+
 
 class GNC:
 
@@ -40,9 +57,8 @@ class GNC:
         self.desat_per_rev = 1.0
         self.fuel_per_desat = 0.042857 # kg
         self.rand_perb = 0
-        self.radius_ship = 2 # meters
-        self.mass_ship = 7000 # kg
-        self.MomInert = (1 / 12) * self.mass_ship * (self.radius_ship ** 2)
+        self.mass_ship = 5945 # kg
+        self.MomDiag = [6821.860, 6533.29, 5245.846] # Ixx, Iyy, Izz format
 
 
 
@@ -58,8 +74,8 @@ class GNC:
         F_G_a = (G * mass_moon * self.mass_ship) / (r_a ** 2)
         min_torque_a = ((0.05 * F_G_a) + F_G_a) * 1  # 1m is assumed min moment arm with a 5% increase in gravity gradient
         max_torque_p = ((0.2 * F_G_p) + F_G_p) * 5  # moment arm is 5m and 20% increase in gravity gradient
-        rand_alpha_a = min_torque_a / self.MomInert  # in radians
-        rand_alpha_p = max_torque_p / self.MomInert
+        rand_alpha_a = min_torque_a / np.linalg.norm(self.MomDiag)  # in radians
+        rand_alpha_p = max_torque_p / np.linalg.norm(self.MomDiag)
         rand_omega_a = rand_alpha_a  # assume the torque is exerted for 1 second
         rand_omega_p = rand_alpha_p  # assume the torque is exerted for 1 second
 
@@ -140,7 +156,7 @@ class GNC:
         for i in range(len(self.omega_vec)):
             self.omega_vec[i] =+ self.base_slew_rate # add an average slew rate each second
             self.omega_vec[i] =+ self.rand_perb # add a random perturbation each second
-            self.lin_mom_vec[i] = self.MomInert*self.omega_vec[i]
+            self.lin_mom_vec[i] = self.MomDiag[i]*self.omega_vec[i]
 
         if np.linalg.norm(self.lin_mom_vec) > self.mom_capacity:
             self.DesatOMM()
