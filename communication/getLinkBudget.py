e3413310aa275d791694f7ae5014c5aa03b6dbbe
Tate Walker <tatewalker@comcast.net>
2020-04-15 22:51:16 -0500

    a lot of big-ass changes

--- communication/getLinkBudget.py
+++ communication/getLinkBudget.py
@@ -5,112 +5,12 @@ import pylink
 import shutil
 import numpy as numpy
 import subprocess
-
+import communication.linkBudgetConstants as lbc
 
 if __name__ == '__main__':
 
-  sat_rf_chain = [
-    pylink.Element(name='Cables',
-                   gain_db=-0.75,
-                   noise_figure_db=0.75),
-    pylink.Element(name='LNA',
-                   gain_db=35,
-                   noise_figure_db=2.75),
-    pylink.Element(name='Filter',
-                   gain_db=-3.5,
-                   noise_figure_db=3.5),
-    pylink.Element(name='Demodulator',
-                   gain_db=0,
-                   noise_figure_db=15),
-    ]
-
-gs_rf_chain = [
-    pylink.Element(name='Cables',
-                   gain_db=-0.75,
-                   noise_figure_db=0.75),
-    pylink.Element(name='LNA',
-                   gain_db=35,
-                   noise_figure_db=2.75),
-    pylink.Element(name='Filter',
-                   gain_db=-3.5,
-                   noise_figure_db=3.5),
-    pylink.Element(name='Demodulator',
-                   gain_db=0,
-                   noise_figure_db=15),
-    ]
-
-  geometry = pylink.Geometry(apoapsis_altitude_km=426452,
-                           periapsis_altitude_km=356873,
-                           min_elevation_deg=20)
-
-  sat_rx_antenna = pylink.Antenna(gain=54.7,
-                                polarization='RHCP',
-                                pattern=sat_pattern,
-                                rx_noise_temp_k=288.84,
-                                is_rx=True,
-                                tracking=False)
-
-  sat_tx_antenna = pylink.Antenna(gain=53.11,
-                                  polarization='RHCP',
-                                  pattern=sat_pattern,
-                                  is_rx=False,
-                                  tracking=False)
-
-  gs_rx_antenna = pylink.Antenna(pattern=pylink.pattern_generator(48),
-                                 rx_noise_temp_k=25.6,
-                                 polarization='LCP',
-                                 is_rx=True,
-                                 tracking=True)
-
-  gs_tx_antenna = pylink.Antenna(gain=79,
-                                 polarization='LCP',
-                                 is_rx=False,
-                                 tracking=True)
-
-  sat_receiver = pylink.Receiver(rf_chain=sat_rf_chain,
-                                 implementation_loss_db=2,
-                                 name='Satellite Ka-Band Receiver')
-
-  gs_receiver = pylink.Receiver(rf_chain=gs_rf_chain,
-                                name='Ground Ka-Band Receiver')
-
-  gs_transmitter = pylink.Transmitter(tx_power_at_pa_dbw=23,
-                                      name='Ground Ka-Band Transmitter')
-
-  sat_transmitter = pylink.Transmitter(tx_power_at_pa_dbw=1.5,
-                                       name='Satellite Ka-Band Transmitter')
-
-  rx_interconnect = pylink.Interconnect(is_rx=True)
-
-
-  tx_interconnect = pylink.Interconnect(is_rx=False)
-
-
-  x_channel = pylink.Channel(bitrate_hz=1e6,
-                             allocation_hz=500e4,
-                             center_freq_mhz=8200,
-                             atmospheric_loss_db=1,
-                             ionospheric_loss_db=1,
-                             rain_loss_db=2,
-                             multipath_fading_db=0,
-                             polarization_mismatch_loss_db=3)
-
-    DOWNLINK = pylink.DAGModel([geometry,
-                              gs_rx_antenna,
-                              sat_transmitter,
-                              sat_tx_antenna,
-                              gs_receiver,
-                              x_channel,
-                              rx_interconnect,
-                              tx_interconnect,
-                              modulation,
-                              pylink.LinkBudget(name='Example XBand Downlink',
-                                                is_downlink=True)])
-
   
-    # We use 'm' here because it is fewer characters than 'model' and
-    # I'm lazy.
-    m = eg_budgets.DOWNLINK
+    m = lbc.DOWNLINK
 
     # The same here with 'e' instead of 'enum'...'e' also doesn't
     # collide with the 'enum' package name.
@@ -121,7 +21,7 @@ gs_rf_chain = [
     r = pylink.Report(m)
 
     # This example is designed to place the result in the export directory
-    output_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+    output_dir = os.path.join(os.getcwd(),
                               'export')
     output_path = os.path.join(output_dir, 'interference_analysis.tex')
     if not os.path.exists(output_dir):
@@ -134,17 +34,17 @@ gs_rf_chain = [
         
     # ITU-R SF.358-5 Lays out the maximum permissible PFD values for
     # various bands.
-    limits=[(0, -150),
-            (5, -150),
-            (25, -140),
-            (90, -140)]
+    limits=[(0, -115),
+            (5, -115),
+            (25, -105),
+            (90, -105)]
 
     # The Canonical PFD figure is the 4kHz BW (though that is tunable
     # if you so desire) peak PFD at the Earth's surface (ie max xmit
     # power, peak antenna gain) as a function of elevation angle,
     # ignoring things like rain fade, since this figure is mostly
     # useful for compliance.
-    title = 'Peak PFD at Surface (4kHz BW)'
+    title = 'Peak PFD at Surface (1MHz BW)'
     canonical_pfd = pylink.CanonicalPFDFigure(m,
                                               title=title,
                                               pfd_limits=limits)
@@ -152,7 +52,7 @@ gs_rf_chain = [
     # Expected PFD is more complicated, as it assumes normal satellite
     # operations (ie Nadir pointing for example), and real antenna
     # patterns instead of peak gain, etc.
-    title = 'Expected PFD at Surface (4kHz BW)'
+    title = 'Expected PFD at Surface (1kHz BW)'
     expected_pfd = pylink.ExpectedPFDFigure(m,
                                             title=title,
                                             pfd_limits=limits)
@@ -226,12 +126,12 @@ gs_rf_chain = [
 
     # Now that we've lined everything up, it's time to build some LaTeX
     r.to_latex(output_path,
-               author='Harrison Caudill',
-               intro='\\input{intro}',
+               author='Tate Walker',
+               intro='Link Budget Report for PPE Module of Gateway',
                expo='So long, and thanks for all the Fish!',
                rx_pfd_bws=[1e3, 4e3, 1e6],
                gso_pfd_bws=[1e3, 4e3, 1e6],
-               watermark_text='Example',
+               watermark_text='',
                pfd_figures=[canonical_pfd,
                             expected_pfd,
                             rx_bw_pfd,
